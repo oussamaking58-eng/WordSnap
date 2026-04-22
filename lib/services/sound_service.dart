a47bad897a1b178c8e7ab342d35090b830c0fff2
Ayari Oussama <oussamaking58@gmail.com>
2026-04-22 22:47:23 +0100

    fix: replace audioplayers with just_audio to fix iOS module error

--- lib/services/sound_service.dart
+++ lib/services/sound_service.dart
@@ -1,15 +1,13 @@
-import 'package:audioplayers/audioplayers.dart';
+import 'package:just_audio/just_audio.dart';
 
 class SoundService {
   static final SoundService _instance = SoundService._internal();
   factory SoundService() => _instance;
-  
+
   final AudioPlayer _player = AudioPlayer();
   bool _enabled = true;
 
-  SoundService._internal() {
-    _player.setReleaseMode(ReleaseMode.stop);
-  }
+  SoundService._internal();
 
   void setEnabled(bool enabled) => _enabled = enabled;
 
@@ -17,15 +15,17 @@ class SoundService {
     if (!_enabled) return;
     try {
       await _player.stop();
-      await _player.play(AssetSource(path), volume: volume);
+      await _player.setVolume(volume);
+      await _player.setAsset('assets/$path');
+      await _player.play();
     } catch (e) {
       // Ignore audio errors in production
     }
   }
 
   Future<void> playPop() => _play('sounds/pop.mp3', volume: 0.6);
-  Future<void> playTick() => _play('sounds/pop.mp3', volume: 0.3); // Réutilisation pour l'instant
+  Future<void> playTick() => _play('sounds/pop.mp3', volume: 0.3);
   Future<void> playDing() => _play('sounds/ding.mp3');
   Future<void> playTada() => _play('sounds/tada.mp3');
-  Future<void> playError() => _play('sounds/pop.mp3', volume: 1.0); // Placeholder
+  Future<void> playError() => _play('sounds/pop.mp3', volume: 1.0);
 }
